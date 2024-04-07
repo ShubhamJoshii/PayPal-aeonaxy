df4a6cca101acdb349b5ecd861f37f07909cf015
ShubhamJoshii <shubhamjoshii676@gmail.com>
2024-04-08 00:23:34 +0530

    Routing Fixed Error:Onrefreshing 404 error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { lazy, Suspense } from "react";
 import Preloader from "./Components/Preloader";
-import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import { Route, Routes, HashRouter as Router } from "react-router-dom";
 import "./App.css";
 const Header = lazy(() => import("./Components/Header"));
 const FrontPage = lazy(() => import("./Pages/FrontPage"));
 const PageNotFound = lazy(()=> import("./Pages/PageNotFound"));
 function App() {
   return (
-    <Router basename="/PayPal-aeonaxy">
+    <Router>
       <Suspense fallback={<Preloader />}>
         <Header />
         <Routes>
